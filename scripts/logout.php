f6d557aeb4752b9ef78d8dc0f0669cbea02a1baa
Jocelyn <jocelyn.routin@outlook.fr>
2017-09-26 09:37:48 +0200

    Remove
    Removed old project

--- scripts/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-   session_start();
-   if (isset($_COOKIE['user']))
-   {
-      $_SESSION['msg'] = "You have been logged out";
-      setcookie("user", $username, -1, "/");
-   }
-   header('Location:/ICTportal/index.php');
-?>
