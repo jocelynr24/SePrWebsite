7b2a22e712bec964a3977709dfe0ab3b86dc882c
Adrien Cosson <adrien.cosson@sciencespo.fr>
2017-10-09 13:38:49 +0200

    started to add course management in admin section

--- includes/config/config.php
+++ includes/config/config.php
@@ -2,5 +2,9 @@
 	$server = "localhost"; 	// Server name
 	$base = "sepr"; 		// Database name
 	$user = "root"; 		// Username
-	$pass = ""; 			// Password
+	$pass = "root"; 			// Password
+	
+	
+	// Use an empty string as password while using WAMP
+	// Use "root" as password while using MAMP
 ?>
