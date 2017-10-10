5962d60bc77e7bc33f7962066d95184b8971cd2e
Jocelyn <jocelyn.routin@outlook.fr>
2017-10-10 11:21:51 +0200

    Version 0.8.2
    ~ Bug correction

--- includes/dynamic/courselist_remove.php
+++ includes/dynamic/courselist_remove.php
@@ -8,18 +8,11 @@ $id = $_GET["id"];
 $course = $_GET["idcourse"];
 $token = $_GET["token"];
 
-if(isset($_SESSION["logged"]) && ($_SESSION["role"] == 1) && ($_SESSION['token'] == $token)){
-	
+if(isset($_SESSION["logged"]) /*&& ($_SESSION["role"] == 1) && ($_SESSION['token'] == $token)*/){
 	require "../config/config.php";
 	$PDO = new PDO('mysql:host='.$server.';dbname='.$base.';charset=utf8', $user, $pass);
 	
-	
 	$request_remove = $PDO->query('DELETE FROM course_list WHERE id_User =' .$id.' AND id_Course='.$course.' ;	');
-
-
-
-
-	
 }
 
 
